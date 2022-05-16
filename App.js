3a76347b51c7f00547a1934f359abdcce71dee62
Shoaib <shoaibarif244@gmail.com>
2022-05-16 10:31:18 +0000

    commit

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import QR_Code_Generator from './src/QR_Code/QR_Code_Generator'
 import QR_Code_Scanner from './src/QR_Code/QR_Code_Scanner'
 import API_Calling from './src/API_Calling/API_Calling'
-import Stripe from './src/PaymentMethod_Stripe/Stripe'
+import Stripe from './src/PaymentMethod_Stripe/StripePaymentMethod'
 
 export default function App() {
   return (
